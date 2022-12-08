a37bd38cf5db42cb0bb7f709bb4638c823cc308b
Tengfei <mtf123ok@gmail.com>
2022-12-09 00:37:13 +0800

    updating

--- routers/index.js
+++ routers/index.js
@@ -3,6 +3,14 @@
 */
 const path = require("path");
 const express = require("express");
+
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
 const app = express();
 const { router: TitleRouter } = require("./title-router");
 const {
@@ -14,6 +22,7 @@ const {
 const { router: AuthRouter } = require("./auth-router");
 
 // Middleware
+app.use(cors(corsOptions)); // Use this after the variable declaration
 app.use(express.json());
 
 // Routers
