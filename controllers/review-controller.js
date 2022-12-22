879df496d84b4de06d8ed540b2a105be726dadd2
Tengfei <mtf123ok@gmail.com>
2022-12-22 17:21:48 +0800

    adding deleteone

--- controllers/review-controller.js
+++ controllers/review-controller.js
@@ -1,85 +1,101 @@
-const {Review} = require("../models");
+const { Review } = require("../models");
 const httpStatus = require("http-status");
 // const { response } = require("express");
 const dbo = require("../db/conn");
 
-
 const create = async (req, response) => {
-    try{
-        let db_connect=dbo.getDb("movieData");
-        let myobj = {
-            movieId : req.body.movieId,
-            title : req.body.title,
-            items : req.body.items,
-        }
+  try {
+    let db_connect = dbo.getDb("movieData");
+    let myobj = {
+      movieId: req.body.movieId,
+      title: req.body.title,
+      items: req.body.items,
+    };
 
-        db_connect.collection("reviews").insertOne(myobj, function (err, res) {
-            if (err) throw err;
-            response.json(res);
-        })
-
-    }catch(e){
-        console.error(e);
-        response.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
-    }
+    db_connect.collection("reviews").insertOne(myobj, function (err, res) {
+      if (err) throw err;
+      response.json(res);
+    });
+  } catch (e) {
+    console.error(e);
+    response.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
 };
 
 const findAll = async (req, res) => {
-    try{        
-        console.log(req.query);
-        const result = await Review.find(req.query).exec();
-        res.json(result);
-    }catch(e){
-        console.error(e);
-        res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
-    }
+  try {
+    console.log(req.query);
+    const result = await Review.find(req.query).exec();
+    res.json(result);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
 };
 
-const updateOne = async(req, response) => {
-    try{
-        console.log(req.body);
-        let db_connect = dbo.getDb("movieData");
-        let movieId = req.params.movieId;
-        let newReview = {
-            $push: { items: req.body}
-        };
+const updateOne = async (req, response) => {
+  try {
+    console.log(req.body);
+    let db_connect = dbo.getDb("movieData");
+    let movieId = req.params.movieId;
+    let newReview = {
+      $push: { items: req.body },
+    };
 
-        db_connect
-            .collection("reviews")
-            .updateOne({movieId:movieId}, newReview, function (err, res) {
-                if (err) throw err;
-                console.log("1 document updated");
-                response.json(res)
-            })
+    db_connect
+      .collection("reviews")
+      .updateOne({ movieId: movieId }, newReview, function (err, res) {
+        if (err) throw err;
+        console.log("1 document updated");
+        response.json(res);
+      });
+  } catch (e) {
+    console.error(e);
+    response.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+};
 
-    }catch(e){
-        console.error(e);
-        response.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
-    }
-}
-
-const deleteOne = async (req,res) => {
-    try{
-        const movieId = req.params.movieId;
-        const deleted = await Review.deleteOne({movieId: movieId});
-        res.json(deleted);
-    }catch(e){  
-        console.error(e);
-        res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
-    }
-}
+const deleteOne = async (req, response) => {
+  try {
+    let db_connect = dbo.getDb("movieData");
+    console.log(req.params);
+    const movieId = req.params.movieId;
+    console.log(movieId);
+    const userName = req.params.userName;
+    console.log(userName);
+    db_connect
+      .collection("reviews")
+      .updateMany(
+        { movieId: movieId },
+        { $pull: { items: { $elemMatch: { username: userName } } } },
+        { multi: true },
+        function (err, res) {
+          if (err) throw err;
+          console.log("1 document updated");
+          response.json(res);
+        }
+      );
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+};
 
 const findOne = async (req, res) => {
-    try{
-        const movieId = req.params.movieId;
-        const fetched = await Review.findOne({movieId:movieId});
-        res.json(fetched);
-    }catch(e){
-        console.error(e);
-        res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR)
-    }
-}
+  try {
+    const movieId = req.params.movieId;
+    const fetched = await Review.findOne({ movieId: movieId });
+    res.json(fetched);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+};
 
 module.exports = {
-    create, findAll, updateOne, deleteOne, findOne
-}
+  create,
+  findAll,
+  updateOne,
+  deleteOne,
+  findOne,
+};
